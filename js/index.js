016961b3dde793237c2e596eb63bb017c1042550
gevorgyandavit <d.g.gevorgyan@mail.ru>
2023-07-18 23:55:50 +0400

    created dinamic menu card

--- js/index.js
+++ js/index.js
@@ -150,4 +150,68 @@ window.addEventListener("DOMContentLoaded", function () {
         document.body.removeAttribute("style");
     }
     // modal logic end
+
+    // start menu card
+
+    class Menu {
+        constructor(img, alt, title, descr, prise, parentelement) {
+            this.img = img;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.prise = prise;
+            this.parentelement = document.querySelector(parentelement);
+            this.uah = 27;
+            this.changToUAH();
+        };
+
+        changToUAH() {
+            this.prise = parseInt(this.prise * this.uah);
+        };
+
+        HTMLgenerator() {
+            const {img, alt, title, descr, prise, parentelement} = this;
+            const element = document.createElement("div");
+            element.classList.add("menu__item");
+            element.innerHTML = `
+            <img src=${img} alt=${alt}>
+            <h3 class="menu__item-subtitle">${title}</h3>
+            <div class="menu__item-descr">${descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${prise}</span> грн/день</div>
+            </div>
+            `;
+            parentelement.append(element);
+        }
+    };
+    new Menu( 
+        "img/tabs/vegy.jpg",
+        "vegy",
+        "Меню \"Фитнес\"",
+        "Меню \"Фитнес\" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+        8.5,
+        ".menu .container"
+    ).HTMLgenerator();
+
+    new Menu( 
+        "img/tabs/elite.jpg",
+        "elite",
+        "Меню \"Премиум\"",
+        "В меню \"Премиум\" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+        20.4,
+        ".menu .container"
+    ).HTMLgenerator();
+
+    new Menu( 
+        "img/tabs/post.jpg",
+        "post",
+        "Меню \"Постное\"",
+        "Меню \"Постное\" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+        16,
+        ".menu .container"
+    ).HTMLgenerator();
+    
+    // end menu card
 });
